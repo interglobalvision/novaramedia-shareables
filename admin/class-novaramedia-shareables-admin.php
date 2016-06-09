033cdd51eaaa6d60899f86693c0de84488d2117b
Patrick Best <patrickbest@patrickbest.com>
2016-06-08 19:35:45 -0500

    fix tab indentation

--- admin/class-novaramedia-shareables-admin.php
+++ admin/class-novaramedia-shareables-admin.php
@@ -22,83 +22,83 @@
  */
 class Novaramedia_Shareables_Admin {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $novaramedia_shareables    The ID of this plugin.
-	 */
-	private $novaramedia_shareables;
+  /**
+   * The ID of this plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      string    $novaramedia_shareables    The ID of this plugin.
+   */
+  private $novaramedia_shareables;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+  /**
+   * The version of this plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      string    $version    The current version of this plugin.
+   */
+  private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $novaramedia_shareables       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $novaramedia_shareables, $version ) {
+  /**
+   * Initialize the class and set its properties.
+   *
+   * @since    1.0.0
+   * @param      string    $novaramedia_shareables       The name of this plugin.
+   * @param      string    $version    The version of this plugin.
+   */
+  public function __construct( $novaramedia_shareables, $version ) {
 
-		$this->novaramedia_shareables = $novaramedia_shareables;
-		$this->version = $version;
+    $this->novaramedia_shareables = $novaramedia_shareables;
+    $this->version = $version;
 
-	}
+  }
 
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+  /**
+   * Register the stylesheets for the admin area.
+   *
+   * @since    1.0.0
+   */
+  public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Novaramedia_Shareables_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Novaramedia_Shareables_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    /**
+     * This function is provided for demonstration purposes only.
+     *
+     * An instance of this class should be passed to the run() function
+     * defined in Novaramedia_Shareables_Loader as all of the hooks are defined
+     * in that particular class.
+     *
+     * The Novaramedia_Shareables_Loader will then create the relationship
+     * between the defined hooks and the functions defined in this
+     * class.
+     */
 
-		wp_enqueue_style( $this->novaramedia_shareables, plugin_dir_url( __FILE__ ) . 'css/novaramedia-shareables-admin.css', array(), $this->version, 'all' );
+    wp_enqueue_style( $this->novaramedia_shareables, plugin_dir_url( __FILE__ ) . 'css/novaramedia-shareables-admin.css', array(), $this->version, 'all' );
 
-	}
+  }
 
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+  /**
+   * Register the JavaScript for the admin area.
+   *
+   * @since    1.0.0
+   */
+  public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Novaramedia_Shareables_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Novaramedia_Shareables_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    /**
+     * This function is provided for demonstration purposes only.
+     *
+     * An instance of this class should be passed to the run() function
+     * defined in Novaramedia_Shareables_Loader as all of the hooks are defined
+     * in that particular class.
+     *
+     * The Novaramedia_Shareables_Loader will then create the relationship
+     * between the defined hooks and the functions defined in this
+     * class.
+     */
 
-		wp_enqueue_script( $this->novaramedia_shareables, plugin_dir_url( __FILE__ ) . 'js/novaramedia-shareables-admin.js', array( 'jquery' ), $this->version, false );
+    wp_enqueue_script( $this->novaramedia_shareables, plugin_dir_url( __FILE__ ) . 'js/novaramedia-shareables-admin.js', array( 'jquery' ), $this->version, false );
 
-	}
+  }
 
   public function add_admin_menu() {
 
@@ -122,7 +122,7 @@ class Novaramedia_Shareables_Admin {
     );
 
     // Enqueue files for Wire Page
-		add_action( 'load-' . $wire_page, array( $this, 'enqueue_shareable_scripts') );
+    add_action( 'load-' . $wire_page, array( $this, 'enqueue_shareable_scripts') );
 
   }
 
@@ -130,15 +130,15 @@ class Novaramedia_Shareables_Admin {
     // Main Script
 
     // EaselJS library
-		wp_enqueue_script( $this->novaramedia_shareables . '_easeljs', plugin_dir_url( __FILE__ ) . 'js/easeljs.min.js', array( 'jquery' ), $this->version );
+    wp_enqueue_script( $this->novaramedia_shareables . '_easeljs', plugin_dir_url( __FILE__ ) . 'js/easeljs.min.js', array( 'jquery' ), $this->version );
 
     // Class in charge of managing the canvas
-		wp_enqueue_script( $this->novaramedia_shareables . '_shareable_canvas', plugin_dir_url( __FILE__ ) . 'js/shareable-canvas.js', array(), $this->version );
+    wp_enqueue_script( $this->novaramedia_shareables . '_shareable_canvas', plugin_dir_url( __FILE__ ) . 'js/shareable-canvas.js', array(), $this->version );
 
-		//wp_enqueue_script( $this->novaramedia_shareables . '_search_post_field', plugin_dir_url( __FILE__ ) . 'js/search-field.js', array('backbone'), $this->version );
+    //wp_enqueue_script( $this->novaramedia_shareables . '_search_post_field', plugin_dir_url( __FILE__ ) . 'js/search-field.js', array('backbone'), $this->version );
 
     // Shareable main script
-		wp_enqueue_script( $this->novaramedia_shareables . '_shareables_script', plugin_dir_url( __FILE__ ) . 'js/novaramedia-shareables.js', array(), $this->version );
+    wp_enqueue_script( $this->novaramedia_shareables . '_shareables_script', plugin_dir_url( __FILE__ ) . 'js/novaramedia-shareables.js', array(), $this->version );
     wp_localize_script( $this->novaramedia_shareables . '_shareables_script', 'ShareableVars', array(
       'ajaxurl' => admin_url( 'admin-ajax.php' ),
       'pluginurl' => plugin_dir_url(__FILE__) . '../'
@@ -164,7 +164,7 @@ class Novaramedia_Shareables_Admin {
       if( !empty( $post ) ) {
         $response = array(
           'type' => 'success',
-          'postData' => $post[0] 
+          'postData' => $post[0]
         );
 
         $response['postData']->post_image = wp_get_attachment_url( get_post_thumbnail_id( $post[0]->ID, 'full' ) );
