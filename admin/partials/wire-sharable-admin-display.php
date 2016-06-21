1fcb2e965b017e88a9071fb270ad5b32666a029a
Patrick Best <patrickbest@patrickbest.com>
2016-06-20 22:08:51 -0500

    add latest 10 posts select

--- admin/partials/wire-sharable-admin-display.php
+++ admin/partials/wire-sharable-admin-display.php
@@ -11,8 +11,9 @@
  * @package    Novaramedia_Shareables
  * @subpackage Novaramedia_Shareables/admin/partials
  */
-?>
-<?php
+
+$recent_posts = get_posts('posts_per_page=10');
+
 $post_url = !empty($_GET['shareable-post-url']) ? $_GET['shareable-post-url'] : '';
 ?>
 
@@ -22,7 +23,22 @@ $post_url = !empty($_GET['shareable-post-url']) ? $_GET['shareable-post-url'] : 
   <h2>Wire Shareable</h2>
 
   <table class="form-table">
+  <h3>Load data</h3>
     <tbody>
+      <tr>
+        <th scope="row">
+          <label for="shareable-post-latest-select" style="width: 100%;">Latest 10 Posts</label>
+        </th>
+        <td>
+          <select name="shareable-post-latest-select" id="shareable-post-latest-select">
+            <?php
+              foreach($recent_posts as $recent_post) {
+                echo '<option value="' . get_the_permalink($recent_post->ID) . '">' . $recent_post->post_title . '</option>';
+              }
+            ?>
+          </select>
+        </td>
+      </tr>
       <tr>
         <th scope="row">
           <label for="shareable-post-url" style="width: 100%;">Post URL</label>
@@ -37,7 +53,7 @@ $post_url = !empty($_GET['shareable-post-url']) ? $_GET['shareable-post-url'] : 
     <input id="get-post-data" type="submit" class="button button-primary" value="Get data">
   </p>
 
-  <h3>Post data</h3>
+  <h3>Shareable settings</h3>
   <table class="form-table">
     <tbody>
       <tr>
@@ -73,5 +89,5 @@ $post_url = !empty($_GET['shareable-post-url']) ? $_GET['shareable-post-url'] : 
   <hr />
 
   <canvas id="canvas-container" width="1000" height="600"></canvas>
-  
+
 </div>
