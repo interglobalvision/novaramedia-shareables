0381804b32586c9ada5a52947d7cd03afa253069
Patrick Best <patrickbest@patrickbest.com>
2017-04-24 19:58:47 -0500

    add more description to main panel

--- admin/partials/shareables-settings-admin-display.php
+++ admin/partials/shareables-settings-admin-display.php
@@ -17,5 +17,13 @@
 
 <div class="wrap">
   <h1>Novara Media shareables generators</h1>
-  <p>This plugin consists of different panels used to create different types of shareable images from different data sources</p>
+  <p>This plugin consists of different panels used to create different types of shareable images from different data sources. Below is a quick guide on how to use the different generators</p>
+
+  <h2>Post Shareables</h2>
+  <p>This generator is designed to make landscape images that promote specific posts on our website. This in practice means Articles and sometimes Audio content. The data from the post is automatically pulled into the generator and mostly the only value that needs to be edited is the main text content.</p>
+  <p>Generally the aim will be to pull a quote out of an article and make it fit with the largest text style. This tends to generate the most shareable image.</p>
+
+  <h2>Square Shareables</h2>
+  <p>This is a multi-use generator that takes an image that is temporarily uploaded and puts it onto a square cropping canvas. It is possible to add text over the top or just use the generator to properly brand a source image</p>
+
 </div>
